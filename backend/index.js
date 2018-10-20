aa42c519a4855e1ac7f6deec8d0e646ccff9e49c
T.J. Tarazevits <tarazevits@gmail.com>
2018-10-20 14:09:00 -0600

    Basic parsing of lat long at current time from tle records

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const satelliteParser = require('satellite.js');
 
 const tleData = fs.readFileSync(`${__dirname}/data/tle_data.json`);
 
@@ -6,6 +7,43 @@ const tleArr = JSON.parse(tleData);
 
 // console.dir(tleArr);
 
+const appendToFile = (fileName, content) => {
+  fs.appendFileSync(fileName, content);
+}
+
+appendToFile('satellitePositionData.csv', `Object Name,Latitude,Longitude`);
+
 tleArr.map((satellite) => {
-  console.log(`satellite name: ${satellite.OBJECT_NAME}`)
-})
+  const satRecord = satelliteParser.twoline2satrec(satellite.TLE_LINE1, satellite.TLE_LINE2);
+  
+  if (satRecord) {
+    // get positionAndVelocity calc for iteration
+    const positionAndVelocity = satelliteParser.propagate(satRecord, new Date());
+
+    // get position and velocity components
+    const positionEci = positionAndVelocity.position;
+    const velocityEci = positionAndVelocity.velocity;
+
+    if (positionEci) {
+      // get position object
+      const gmst = satelliteParser.gstime(new Date());
+      const positionGd = satelliteParser.eciToGeodetic(positionEci, gmst);
+
+      // get position in radians
+      const longitude = positionGd.longitude;
+      const latitude  = positionGd.latitude;
+      const height    = positionGd.height;
+
+      // convert to lat/long in degress
+      const longitudeStr = satelliteParser.degreesLong(longitude);
+      const latitudeStr  = satelliteParser.degreesLat(latitude);
+
+      console.log(`satellite name: ${satellite.OBJECT_NAME}, lat: ${latitudeStr}, long: ${longitudeStr}`)
+      appendToFile('satellitePositionData.csv', `${satellite.OBJECT_NAME},${latitudeStr},${longitudeStr}\n`);
+    }
+  }
+});
+
+// const satRecord = satelliteParser.twoline2satrec(tleLine1, tleLine2);
+
+// const satRecord = satelliteParser.twoline2satrec()
