7bc0db20dcf3650469232b19ffd4ceaac1cc17a5
T.J. Tarazevits <tarazevits@gmail.com>
2018-10-24 00:18:54 -0600

    Add perPeriod propogation
    Moved sim code to helper function
    
    Created new endpoint to generate one orbit period of sim data.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const express = require('express');
-const { propogate } = require('./propogation');
+const { propogate, propogatePerPeriod } = require('./propogation');
+const { getTLEList } = require('./tle');
+
 const app = express();
 const port = process.env.PORt ? process.env.PORT : 4000;
 
@@ -24,18 +26,53 @@ params:
 - step: millis
 */
 app.get('/propogate', async (req, res) => {
-
   const { satelliteID, start, end, step } = req.query;
-
   if (!satelliteID || !start || !end || !step) {
     res.status(400).send({ error: 'Missing required query params' });
   }
   // additional per-param validation here [TODO]
-
   const propogationResults = await propogate(satelliteID, Number.parseInt(start, 10),  Number.parseInt(end, 10),  Number.parseInt(step, 10));
-
   res.send(propogationResults);
 
+});
+
+/*
+params:
+- satelliteID: catalog string
+*/
+app.get('/propogatePeriod', async (req, res) => {
+  const { satelliteID, periods } = req.query;
+  if (!satelliteID) {
+    res.status(400).send({ error: 'Missing required query params' });
+  }
+  const simPeriods = periods ? periods : 1; // if not given, simulate for one orbit period
+  const propogationResults = await propogatePerPeriod(satelliteID, simPeriods)
+  res.send(propogationResults);
+});
+
+/*
+params
+- satelliteID: catalog string
+*/
+app.get('/satelliteInfo', async (req, res) => {
+  const tleList = await getTLEList();
+  const satellite = tleList.find(tle => tle.NORAD_CAT_ID);
+  const satelliteInfo = {
+      satelliteID: Number.parseInt(satellite.NORAD_CAT_ID, 10),
+      name: satellite.OBJECT_NAME,
+      type: satellite.OBJECT_TYPE,
+      classification: satellite.CLASSIFICATION_TYPE,
+      apogee: Number.parseFloat(satellite.APOGEE, 10),
+      perigee: Number.parseFloat(satellite.PERIGEE, 10),
+      period: Number.parseFloat(satellite.PERIOD, 10),
+    };
+  res.send(satelliteInfo);
+});
+
+app.get('/satelliteIDs', async (req, res) => {
+  const tleList = await getTLEList();
+  const satelliteIDs = tleList.map( tle => tle.NORAD_CAT_ID);
+  res.send(satelliteIDs);
 });
 
 app.listen(port, () => console.log(`Sat-Track Server is listening on port ${port}`));
--- backend/propogation.js
+++ backend/propogation.js
@@ -1,12 +1,30 @@
 const satelliteParser = require('satellite.js');
 const { getTLEList } = require('./tle');
 
+const propogatePerPeriod = async (satelliteID, periodsToSim) => {
+  const tleList = await getTLEList();  
+  const satellite = tleList.find(tle => tle.NORAD_CAT_ID);
+  if (!satellite) {
+    return ({
+      error: "No satellite with that ID was found in the catalog",
+      satelliteID,
+    });
+  }
+  const satRecord = satelliteParser.twoline2satrec(satellite.TLE_LINE1, satellite.TLE_LINE2);
+  let predictions = [];
+  const start = Date.now();
+  const periodMinutes = Number.parseInt(satellite.PERIOD, 10);
+  const end = start + ((periodMinutes * 60 * 1000) * periodsToSim);
+  const step = (end - start) / (1000 * periodsToSim); // create 1000 iterations for each period; keeps data size down, roughly
+  if (satRecord) {
+    predictions = await propogationSim(satRecord, start, end, step)
+  }
+  return predictions;
+}
 
 const propogate = async (satelliteID, start, end, step) => {
   const tleList = await getTLEList();  
-
   const satellite = tleList.find(tle => tle.NORAD_CAT_ID);
-
   if (!satellite) {
     return ({
       error: "No satellite with that ID was found in the catalog",
@@ -15,47 +33,50 @@ const propogate = async (satelliteID, start, end, step) => {
   }
 
   const satRecord = satelliteParser.twoline2satrec(satellite.TLE_LINE1, satellite.TLE_LINE2);
-  const predictions = [];
+  let predictions = [];
   if (satRecord) {
-    const startOfPropagation = start;
-    let numIterations = 0;
-    for (let i = startOfPropagation; i < end; i += step) {
-      if (numIterations % 10 === 0) console.log(`starting iteration: ${numIterations}`);
-      const futureTime = new Date(i);
-      const positionAndVelocity = satelliteParser.propagate(satRecord, futureTime);
-      // get position and velocity components
-      const positionEci = positionAndVelocity.position;
-      const velocityEci = positionAndVelocity.velocity;
-
-      if (positionEci) {
-        // get position object
-        const gmst = satelliteParser.gstime(futureTime);
-        const positionGd = satelliteParser.eciToGeodetic(positionEci, gmst);
-
-        // get position in radians
-        const longitude = positionGd.longitude;
-        const latitude  = positionGd.latitude;
-        const height    = positionGd.height;
-
-        // convert to lat/long in degress
-        const longitudeStr = satelliteParser.degreesLong(longitude);
-        const latitudeStr  = satelliteParser.degreesLat(latitude);
-
-        // console.log(`time in the future: ${futureTime}, lat: ${latitudeStr}, long: ${longitudeStr}`)
-        // appendToFile('satelliteIterativePlot.csv', `${satellite.OBJECT_NAME},${futureTime},${latitudeStr},${longitudeStr}\n`);
-        predictions.push({timestamp: futureTime, lat: latitudeStr, long: longitudeStr});
-      }
-      numIterations++;
-    }
+    predictions = await propogationSim(satRecord, start, end, step)
   }
-
   return predictions;
+}
 
+const propogationSim = async (satRecord, startOfPropagation, endOfPropogation, step) => {
+  let numIterations = 0;
+  const predictions = [];
+  for (let i = startOfPropagation; i < endOfPropogation; i += step) {
+    if (numIterations % 10 === 0) console.log(`starting iteration: ${numIterations}`);
+    const futureTime = new Date(i);
+    const positionAndVelocity = satelliteParser.propagate(satRecord, futureTime);
+    // get position and velocity components
+    const positionEci = positionAndVelocity.position;
+    const velocityEci = positionAndVelocity.velocity;
 
+    if (positionEci) {
+      // get position object
+      const gmst = satelliteParser.gstime(futureTime);
+      const positionGd = satelliteParser.eciToGeodetic(positionEci, gmst);
+
+      // get position in radians
+      const longitude = positionGd.longitude;
+      const latitude  = positionGd.latitude;
+      const height    = positionGd.height;
+
+      // convert to lat/long in degress
+      const longitudeStr = satelliteParser.degreesLong(longitude);
+      const latitudeStr  = satelliteParser.degreesLat(latitude);
+
+      // console.log(`time in the future: ${futureTime}, lat: ${latitudeStr}, long: ${longitudeStr}`)
+      // appendToFile('satelliteIterativePlot.csv', `${satellite.OBJECT_NAME},${futureTime},${latitudeStr},${longitudeStr}\n`);
+      predictions.push({timestamp: futureTime, lat: latitudeStr, long: longitudeStr});
+    }
+    numIterations++;
+  }
+  return predictions;
 }
 
 
 
 module.exports = {
-  propogate
+  propogate,
+  propogatePerPeriod
 };
